7aa6fba107b5aeb2358f2fda63b27db729266c55
fabiolucasz <fabio_9.9@hotmail.com>
2025-06-15 19:41:20 -0300

    update

--- extract_all_fiis.py
+++ extract_all_fiis.py
@@ -8,7 +8,7 @@ headers = {
 
 
 
-def fundamentus_craw():
+def get_fiis():
     url = "https://www.fundamentus.com.br/fii_resultado.php"
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -16,11 +16,49 @@ def fundamentus_craw():
     df = pd.read_html(str(table))[0]
     return df
 
+def get_data_com(papel: str):
+    url = f"https://investidor10.com.br/fiis/{papel.lower()}/"
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find_all('table', class_="table")
+    #tabela datacom
+    #table = soup.find_all('table', class_="table-balance")
+    df = pd.read_html(str(table))[0]
+    return df
 
 
-fundamentus_craw().to_csv("fiis.csv", index=False)
+def get_fiis_complement(papel: str):
+    url = f"https://investidor10.com.br/fiis/{papel.lower()}/"
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    titulos = [item.text.strip() for item in soup.find_all('span', class_="name")]
+    valores = [item.text.strip() for item in soup.find_all('div', class_="value")]
+    
+    dados = dict(zip(titulos, valores))
+    dados["Papel"] = papel
+
+    return dados
+
+fiis =pd.read_csv("fiis.csv", quotechar='"', sep=',', decimal='.', encoding='utf-8', skipinitialspace=True)
+#tickers = fiis["Papel"].tolist()
 
 
+#dados_fiis = []
+#for ticker in tickers:
+#    dados = get_fiis_complement(ticker)
+#    if dados:
+#        dados_fiis.append(dados)
 
+# Converte para DataFrame e salva
+#df = pd.DataFrame(dados_fiis)
+#df.to_csv("dados_investidor10.csv", index=False)
 
+df2 = pd.read_csv("dados_investidor10.csv")
 
+#concat = pd.concat([fiis, df2], ignore_index=True)
+#concat.to_csv("concat.csv", index=False)
+
+merge = pd.merge(fiis, df2, on="Papel", how="inner")
+merge.to_csv("merge.csv", index=False)
